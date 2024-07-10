b093403083001c2ffd567a2ac3e7352f7359a623
VictorTimbo <manoelvictortimbo@gmail.com>
2024-07-10 11:09:21 -0300

    feat: create navigation bar style

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
-import './assets/main.css'
+import '@/style.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+
+import PrimeVue from 'primevue/config'
+import Aura from "@primevue/themes/aura"
+
 import router from './router'
 
+import { OhVueIcon, addIcons } from 'oh-vue-icons'
+import { RiHeartFill, RiHeartAddLine } from "oh-vue-icons/icons";
+
+addIcons(RiHeartFill, RiHeartAddLine);
+
 const app = createApp(App)
-
+app.component("v-icon", OhVueIcon);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+})
 app.use(router)
-
 app.mount('#app')
