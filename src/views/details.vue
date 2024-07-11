6d7d9b7e4ffe8295385ad43aeb2ff4381a0465a0
Pedro Geovani <pedro.geovanisc@gmail.com>
2024-07-11 15:55:36 -0300

    add components

--- src/views/details.vue
+++ src/views/details.vue
@@ -3,5 +3,26 @@ export default {}
 </script>
 
 <template>
-    
+  <div class="card">    
+    <div class="transparence">
+      <div class="conteiner"> 
+      <div>
+        <p class="title"> Titulo do video </p>
+      </div>
+      <div class="textsmall"> 
+        <a> 2014 </a>
+        <a> 7.5 </a> | 
+        <a> 110 min </a>
+      </div>
+      <div class="textgroung"> 
+        <p> "Este filme acontece na codade de Trana durante uma guerra civil entre dois grupos pro liberdade"</p>
+      </div>
+    </div>
+      <div>
+        <iframe class="video" width="853" height="480" src="https://www.youtube.com/embed/lCemyQeSCV8" title="CURSO DE TYPESCRIPT NA PRÁTICA - APRENDA TYPESCRIPT EM 1 HORA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>">
+      </div>
+
+    </div>
+  </div> 
+
 </template>
