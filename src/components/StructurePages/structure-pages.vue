479c5f308266ec8a2f0721be276dfbfbe0126803
Gabriel <mxbiel2000@gmail.com>
2024-07-12 13:51:27 -0300

    tailwind implementation

--- src/components/StructurePages/structure-pages.vue
+++ src/components/StructurePages/structure-pages.vue
@@ -1,45 +1,33 @@
 <script lang="ts">
-    export default{
-        name: "StructurePages"
-    }
+export default {
+    name: "StructurePages"
+}
 </script>
 
 <template>
-    <main>
-        <div class="w-screen h-screen  bg-black">      <!-- conteiner -->
-            <div class="flex justify-center items-center">    <!-- scrollImages -->
-                <div class="allImages inline-flex bg-gray-700 p-28">
-                    <div class="cardImages p-16 bg-blue-600 m-5">FILME 1</div>
-                    <div class="cardImages p-16 bg-blue-600 m-5">FILME 2</div>
-                    <div class="cardImages p-16 bg-blue-600 m-5">FILME 3</div>
+    <body class="m-0 p-0">
+        <div class="min-h-screen justify-between flex-wrap bg-gray-800">
+            <!-- conteiner -->
+            <div class="flex justify-center items-center pt-8">
+                <!-- scrollImages -->
+                <div class="allImages inline-flex bg-gray-700 py-14 px-28">
+                    <div class="cardImages p-16 bg-blue-600 m-5 w-[304px] h-[456px] rounded-2xl">FILME 1</div>
+                    <div class="cardImages p-16 bg-blue-600 m-5 w-[304px] h-[456px] rounded-2xl">FILME 2</div>
+                    <div class="cardImages p-16 bg-blue-600 m-5 w-[304px] h-[456px] rounded-2xl">FILME 3</div>
                 </div>
             </div>
+    
+            <div class="flex justify-center items-center">
+                <!-- scrollImages -->
+                <div class="allImages inline-flex bg-gray-700 py-14 px-28">
+                    <div class="cardImages p-16 bg-blue-600 m-5 w-[304px] h-[456px] rounded-2xl">FILME 1</div>
+                    <div class="cardImages p-16 bg-blue-600 m-5 w-[304px] h-[456px] rounded-2xl">FILME 2</div>
+                    <div class="cardImages p-16 bg-blue-600 m-5 w-[304px] h-[456px] rounded-2xl">FILME 3</div>
+                </div>
+            </div>
+            <div class="flex justify-center items-center p-8">
+                <p class="text-white text-center">1 ... 500</p>
+            </div>
         </div>
-    </main>
+    </body>
 </template>
-
-<style>
-/* .scrollImages {
-    background-color: rgb(42, 41, 41);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-    width: 100%;
-}
-
-.allImages {
-    background-color: rgb(54, 54, 54);
-    padding: 25px 150px 50px;
-    display: inline-flex;
-}
-
-.cardImages {
-    padding: 100px;
-    background-color: brown;
-    margin: 20px;
-    width: 304px;
-    height: 456px;
-    border-radius: 20px;
-} */
-</style>
