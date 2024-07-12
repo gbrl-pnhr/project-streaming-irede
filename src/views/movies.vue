b85192e7e1b69d02bdbc53fb3a748c305b7a5929
victor <manoelvictortimbo@gmail.com>
2024-07-12 16:58:21 -0300

    feat: get api data in movies view

--- src/views/movies.vue
+++ src/views/movies.vue
@@ -1,5 +1,34 @@
 <script lang="ts">
-export default {}
+import { StreamingService } from '@/views/streaming.service';
+import { Streamings } from '@/models/streaming.model';
+export default {
+    data() {
+        return {
+            films: new Array<Streamings>(),
+            page: 1,
+        }
+    },
+    mounted() {
+        this.getMovies();
+    },
+    computed: {
+        movieService(): StreamingService {
+            return new StreamingService();
+        }
+    },
+    methods: {
+        getMovies(page?: number) {
+            this.movieService.streamings
+                .subscribe({
+                    next: (response: any) => {
+                        this.films = response.results;
+                        console.log(response.results)
+                    }
+                });
+            this.movieService.getMovies();
+        }
+    },
+}
 </script>
 
 <template>
