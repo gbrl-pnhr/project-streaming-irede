25e3d4c5cabceaf5adcf79cbce784f739ad0fae6
Pedro Geovani <pedro.geovanisc@gmail.com>
2024-07-14 13:14:03 -0300

    feature: Update responsivity

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,26 @@
-import '@/style.css'
-
+import { RiHeartFill, RiHeartAddLine, HiMenu } from "oh-vue-icons/icons";
+import { OhVueIcon, addIcons } from 'oh-vue-icons'
+import { setComponent } from './modules/component.module';
 import { createApp } from 'vue'
-import App from './App.vue'
+import ToggleSwitch from 'primevue/toggleswitch';
 import PrimeVue from 'primevue/config'
 import Button from 'primevue/button';
 import router from './router'
-import { OhVueIcon, addIcons } from 'oh-vue-icons'
-import { RiHeartFill, RiHeartAddLine, HiMenu } from "oh-vue-icons/icons";
-import { setComponent } from './modules/component.module';
+import Image from 'primevue/image';
 import Card from 'primevue/card';
-import ToggleSwitch from 'primevue/toggleswitch';
 import Aura from '@primevue/themes/aura';
+import App from './App.vue'
+import { Theme } from "@primevue/themes";
 
 addIcons(RiHeartFill, RiHeartAddLine, HiMenu);
 
 const app = createApp(App);
 setComponent(app);
-app.use(PrimeVue, {
-    unstyled: true
-})
+app.use(PrimeVue, { unstyled: true });
 app.use(router);
 app.component("v-icon", OhVueIcon);
 app.component("Button", Button);
 app.component("Card", Card);
 app.component("ToggleSwitch", ToggleSwitch);
+app.component("Image", Image);
 app.mount('#app');
--- src/views/details.vue
+++ src/views/details.vue
@@ -1,42 +1,29 @@
 <template>
-  <div class="background min-height-full; max-w-full">
-    <div class="transparency" >
-      <Card>
-          <template #title>
-            <div class="text-[48px] font-normal md:font-bold">
-              {{ titleMovie }}
-            </div> 
-          </template>
-
-        <template #subtitle>  
-          <div class="text-[16px]">
+  <div id="imageBackground" class="bg-no-repeat bg-scroll bg-cover bg-center">
+    <div class="bg-gradient-to-r from-black to-transparent">
+      <div class="block lg:inline-block h-screen collumns-1 lg:columns-2">        
+        <div class="block p-4 lg:p-24 lg:inline-block">
+          <div class="text-[36px] font-extrabold text-white lg:text-[48px]">
+            {{ titleMovie }}
+          </div>          
+          <div class="text-[16px] text-white lg:text-[18px]">
             <a> {{ year }} &nbsp | </a>
             <a style="color: rgb(20,120,0);">&nbsp {{ assessment }} &nbsp </a> 
             <a>| &nbsp {{ duration }} </a>
-          </div>   
-        </template>
-
-        <template #content>            
-              <div class="text-[24px]">
-                {{ sinopse }} 
-              </div> 
-        </template>
-          
-        <template #footer> 
-          <ToggleSwitch  v-model="checked"> Favorito </ToggleSwitch> 
-        </template>
-      </Card>
-
-
-       
-         <iframe style="border-radius: 40px; scale: 50%;" width="773" height="435" :src="urlMovie" title="Moana Official Trailer" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-    
-      
-        
-   
-  
-         </div>
+          </div>          
+          <div class="text-[18px] text-white text-justify lg:text-[24px]">
+            {{ sinopse }}
+          </div> 
+          <div>
+            <Button label="click"> </Button>
+          </div>
+        </div>
+        <div class="flex justify-center lg:inline-block lg:mt-56 lg:mx-24" >
+          <iframe class=" border-2 rounded-lg " :src="urlMovie" title="Moana Official Trailer" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div> 
+      </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -45,33 +32,26 @@ export default{
   data(){
     return{
       titleMovie: 'Moana',
-      urlBackground: 'https://s2.glbimg.com/cic0sVOCxbZM6NZi6urWWKUG04M=/s.glbimg.com/jo/g1/f/original/2015/10/08/moana-disney.jpg',
-      sinopse: 'Uma jovem parte em uma missão para salvar seu povo. Durante a jornada, Moana conhece o outrora poderoso semideus Maui, que a guia em sua busca para se tornar uma mestre em encontrar caminhos. Juntos, eles navegam pelo oceano em uma viagem incrível.',
       year: '2014',
-      assessment: '7.5',
       duration: '120 min',
+      assessment: '7.5',
+      sinopse: 'Uma jovem parte em uma missão para salvar seu povo. Durante a jornada, Moana conhece o outrora poderoso semideus Maui, que a guia em sua busca para se tornar uma mestre em encontrar caminhos. Juntos, eles navegam pelo oceano em uma viagem incrível.',
+      urlBackground: 'https://s2.glbimg.com/cic0sVOCxbZM6NZi6urWWKUG04M=/s.glbimg.com/jo/g1/f/original/2015/10/08/moana-disney.jpg',
       urlMovie: "https://www.youtube.com/embed/LKFuXETZUsI",
-      checked: true,     
+      favorite: false,     
     }
+  },
+  mounted(){
+    this.setBackground();
+    
+
+  },
+  methods:{
+    setBackground(){
+      document.getElementById("imageBackground").style.backgroundImage = `url("${this.urlBackground}")`;
+    },
   }
 }
 </script>
-
-<style scoped>
-.background{
-  background-image: url( 'https://s2.glbimg.com/cic0sVOCxbZM6NZi6urWWKUG04M=/s.glbimg.com/jo/g1/f/original/2015/10/08/moana-disney.jpg');
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-size: cover;
-  background-position: center center;
-}
-
-.transparency{
-  columns: 2;
-  background-image: linear-gradient(to right, rgba(0,0,0,1),rgba(0,0,0,0.8), rgba(0, 0, 0, 0));
-  color: rgb(255, 255, 255);
-  padding: 100px;
-}
-</style>
 
 
