05921d9f015d8e96fdb0ae80c5a7b9712cc461ab
victor <manoelvictortimbo@gmail.com>
2024-07-16 13:17:02 -0300

    refactor: remove console.log in service

--- src/models/streaming.model.ts
+++ src/models/streaming.model.ts
@@ -3,24 +3,30 @@ export class Streamings {
         public adult?: boolean,
         public backdrop_path?: string,
         public id?: number,
-        public name?: string, // series
-        public title?: string, // filmes
+        public name?: string,
+        public title?: string, 
         public overview?: string,
         public poster_path?: string,
         public media_type?: string,
         public genre_ids?: number[],
         public popularity?: number,
-        public release_date?: string, // filmes
-        public first_air_date?: string, // series
-        public videos?: Videos[]
+        public release_date?: string,
+        public first_air_date?: string,
+        public videos?: Videos[],
+        public runtime?: number,
+        public number_of_episodes?: number,
+        public number_of_seasons?: number,
     ) {}
 }
 
 class Videos {
     constructor(
-        public name_video?: string,
-        public key?: string,
-        public size?: 360,
-        public type?: string,
+        public results?: [
+            {
+                name_video?: string,
+                key?: string,
+                type?: string,
+            }
+        ]
     ) {}
 }
--- src/views/streaming.service.ts
+++ src/views/streaming.service.ts
@@ -14,7 +14,6 @@ export class StreamingService {
             .subscribe({
                 next: (response: any) => {
                     this.streamings$.next(response);
-                    console.log(response.results);
                 },
                 error: () => {
                     alert("Não foi possível recuperar os dados das Series e Filmes");
@@ -28,7 +27,6 @@ export class StreamingService {
             .subscribe({
                 next: (response: any) => {
                     this.streamings$.next(response);
-                    console.log(response.results);
                 },
                 error: () => {
                     alert("Não foi possível recuperar os dados dos Filmes");
@@ -42,7 +40,6 @@ export class StreamingService {
             .subscribe({
                 next: (response: any) => {
                     this.streamings$.next(response);
-                    console.log(response.results);
                 },
                 error: () => {
                     alert("Não foi possível recuperar os dados das Series");
@@ -56,7 +53,6 @@ export class StreamingService {
             .subscribe({
                 next: (response: any) => {
                     this.streamings$.next(response);
-                    console.log(response);
                 },
                 error: () => {
                     alert("Não foi possível recuperar o Trailer");
